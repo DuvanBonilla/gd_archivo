73abfd033bfe80157bd11d01c50b2573670afbe2
kenier <kenier202@gmail.com>
2024-08-01 15:42:16 -0500

    feat(session): se agrego el uso de sesiones

--- view/index.php
+++ view/index.php
@@ -1,7 +1,15 @@
+<?php
+session_start();
+if(!isset($_SESSION["Usuario"])){
+  header('location: ../view/login.php');
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="icon" href="images/logo.ico.ico" type="image/x-icon" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu Dashboard</title>
--- view/login.php
+++ view/login.php
@@ -2,6 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
+    <link rel="icon" href="images/logo.ico.ico" type="image/x-icon" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script
       src="https://kit.fontawesome.com/64d58efce2.js"
