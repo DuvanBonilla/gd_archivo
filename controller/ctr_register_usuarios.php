84fd2228b05c9745d5b53c205c37dbb585f8a4c7
kenier <kenier202@gmail.com>
2024-08-15 17:20:10 -0500

    refactor(controller): se refactorizo el controlador de registro

--- controller/ctr_register_usuarios.php
+++ controller/ctr_register_usuarios.php
@@ -1,11 +1,9 @@
 <?php
-
 session_start();
 if (!isset($_SESSION['usuario'])) {
     header('location: ../view/login.php');
     exit;
 }
-
 require_once '../model/conexion.php';
 require_once '../model/val_register.php';
 
@@ -21,33 +19,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $zona = 1;
         $usuario = $_POST['usuario'];
         $contrasena = $_POST['contrasena'];
-
+        $areasSeleccionadas = $_POST['areas'];
         // Obtener las empresas seleccionadas y sus estados
-        // $empresasSeleccionadas = isset($_POST['empresa']) ? $_POST['empresa'] : [];
         $empresaEstados = isset($_POST['empresaEstado']) ? $_POST['empresaEstado'] : [];
 
-        $empresasConEstados = [];
-        foreach ($empresaEstados as $empresaId => $estado) {
-            // Agregar cada empresa con su estado, si no existe un estado se asigna '2' por defecto
-            $estado = !empty($estado) ? $estado : '2';
-            $empresasConEstados[] = [
-                'id' => $empresaId,
-                'estado' => $estado,
-            ];
-        }
-
-        if (isset($_POST['areas'])) {
-            $areasSeleccionadas = $_POST['areas'];
-            $areasConEstados = [];
-
-            foreach ($areasSeleccionadas as $idArea => $valorSeleccionado) {
-                $areasConEstados[] = [
-                    'id' => $idArea,
-                    'estado' => $valorSeleccionado,
-                ];
-            }
-        }
         $valRegister = new val_register_personas();
-        $valRegister->AggUser($cedula, $nombre, $razon_social, $rol, $zona, $usuario, $contrasena, $empresasConEstados, $areasConEstados, $conMysql);
+        $empresasConEstados = $valRegister->ArrayEmpresaEstado($empresaEstados);
+        $areasConEstados=$valRegister->Areas($areasSeleccionadas);
+
+        try{
+            $valRegister->AggUser($cedula, $nombre, $razon_social, $rol, $zona, $usuario, $contrasena, $empresasConEstados, $areasConEstados, $conMysql);
+
+        }catch(Exception $e){
+            echo "Error al insertar" . $e->getMessage();
+        }
     }
+}else{
+    echo "datos incompletos";
 }
