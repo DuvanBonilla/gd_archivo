b5a06bb8c0eeb1846fa383b8e6d873b439422d7f
kenier <kenier202@gmail.com>
2024-08-20 09:59:13 -0500

    feat(permisos): se creo la consulta de permisos

---
+++ model/consu_permisos_usuario.php
@@ -0,0 +1,43 @@
+<?php
+include 'conexion.php';
+
+$cedular = $_SESSION["cedula"];
+class Permisos
+{
+    private $conexion;
+
+    public function __construct($conexion)
+    {
+        $this->conexion = $conexion;
+    }
+
+    public function obtenerPermisos($cedula)
+    {
+        $consulta = "SELECT Area, Permiso FROM tbl_accesos WHERE Cedula = '$cedula'";
+        
+        $resultado = mysqli_query($this->conexion, $consulta);
+
+        if (!$resultado) {
+            exit('Error en la consulta: '.mysqli_error($this->conexion));
+        }
+
+        $cedula = [];
+        while ($fila = mysqli_fetch_assoc($resultado)) {
+            $cedula[] = $fila;
+        }
+
+        return $cedula;
+    }
+}
+
+// Crear una instancia de la conexión
+$conexion = (new Conexion())->conMysql();
+// Crear una instancia de la clase Permisos y obtener los datos
+$empleadosClass = new Permisos($conexion);
+$permiso = $empleadosClass->obtenerPermisos($cedular);
+
+// Cerrar la conexión
+(new Conexion())->cerrarConexion($conexion);
+
+// Retornar los datos
+return $permiso;
